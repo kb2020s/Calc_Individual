2d325f331026f6ba5d29d675d886da11fd247232
Kamal Bhagat <kjb24@njit.edu>
2020-03-06 19:23:18 -0500

    Add unit tests for multiplication and Division

--- CalcTests.py
+++ CalcTests.py
@@ -21,5 +21,20 @@ class TestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+    def test_multiplication(self):
+        test_data = CsvReader('/src/UnitTestMultiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_division(self):
+        test_data = CsvReader('/src/UnitTestDivision.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.divide(row['Value 2'], row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+        
 if __name__ == '__main__':
     unittest.main()
